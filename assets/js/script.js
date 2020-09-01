ef84c279fa7d9d158b70f498544dae67925f80e4
cornwalljeremy <cornwall.jeremy@gmail.com>
2020-09-01 12:45:34 -0600

    date-change

--- assets/js/script.js
+++ assets/js/script.js
@@ -141,27 +141,20 @@ $(".list-group").on("click", "span", function () {
   dateInput.trigger("focus");
 });
 // value of due date was changed
-$(".list-group").on("change", "input[type='text']", function () {
-  // get current text
-  var date = $(this).val().trim();
+$(".list-group").on("change", "input[type='text']", function() {
+  var date = $(this).val();
 
-  // get the parent ul's id attribute
   var status = $(this).closest(".list-group").attr("id").replace("list-", "");
-
-  // get the task's position in the list of other li elements
   var index = $(this).closest(".list-group-item").index();
 
-  // update task in array and re-save to localstorage
   tasks[status][index].date = date;
   saveTasks();
 
-  // recreate span element with bootstrap classes
-  var taskSpan = $("<span>")
-    .addClass("badge badge-primary badge-pill")
-    .text(date);
-
-  // replace input with span element
+  var taskSpan = $("<span>").addClass("badge badge-primary badge-pill").text(date);
   $(this).replaceWith(taskSpan);
+
+  // Pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 $(".card .list-group").sortable({
