ce3a1106e58140d3daa3e52e689c242ce4bdcb2c
cornwalljeremy <cornwall.jeremy@gmail.com>
2020-08-31 18:08:00 -0600

    dragging

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,7 +31,7 @@ var loadTasks = function () {
   // loop over object properties
   $.each(tasks, function (list, arr) {
     // then loop over sub-array
-    arr.forEach(function(task) {
+    arr.forEach(function (task) {
       createTask(task.text, task.date, list);
     });
   });
@@ -103,7 +103,7 @@ $("#task-form-modal .btn-primary").click(function () {
 });
 
 // remove all tasks
-$("#remove-tasks").on("click", function() {
+$("#remove-tasks").on("click", function () {
   for (var key in tasks) {
     tasks[key].length = 0;
     $("#list-" + key).empty();
@@ -150,6 +150,66 @@ $(".list-group").on("blur", "input[type='text']", function () {
 
   // replace input with span element
   $(this).replaceWith(taskSpan);
+});
+
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function (event) {
+    console.log("activate", this);
+  },
+  deactivate: function (event) {
+    console.log("deactivate", this);
+  },
+  over: function (event) {
+    console.log("over", event.target);
+  },
+  out: function (event) {
+    console.log("out", event.target);
+  },
+  update: function (event) {
+    var tempArr = [];
+
+    // loop over current set of children in sortable list
+    $(this)
+      .children()
+      .each(function () {
+        var text = $(this).find("p").text().trim();
+
+        var date = $(this).find("span").text().trim();
+
+        // add task data to the temp array as an object
+        tempArr.push({
+          text: text,
+          date: date,
+        });
+        
+      });
+      // trim down list's ID to match object property
+      var arrName = $(this).attr("id").replace("list-", "");
+
+      // update array on tasks object and save
+      tasks[arrName] = tempArr;
+      saveTasks();
+
+    console.log(tempArr);
+  },
+});
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function (event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function (event, ui) {
+    console.log("over");
+  },
+  out: function (event, ui) {
+    console.log("out");
+  },
 });
 
 // load tasks for the first time
